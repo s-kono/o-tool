bce3c3e16c411cb9d39a0f3fbe8fa4a026c673f1
s-kono <s-kono@outlook.com>
2019-05-06 04:27:41 +0900

    .

--- jpholiday.sh
+++ jpholiday.sh
@@ -2,7 +2,8 @@
 
 set -u
 
-export LANG=ja_JP.UTF-8
+export LANG=C
+export LC_CTYPE=en_US.UTF-8
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 readonly URL_iCalGoogle_JHoliday="https://calendar.google.com/calendar/ical/ja.japanese%23holiday@group.v.calendar.google.com/public/basic.ics"
@@ -13,7 +14,7 @@ readonly URL_csvCAO_JHoliday=http://www8.cao.go.jp/chosei/shukujitsu/syukujitsu_
 
 readonly TODAY_YYYYMMDD=$( date +%Y%m%d )
 
-curl --retry 2 --retry-delay 8 --max-time 5 -sS "${URL_iCalGoogle_JHoliday}" \
+curl -sSm 5 "${URL_iCalGoogle_JHoliday}" \
 | tr -d '\r' \
 | grep -E "^(DTSTART|SUMMARY)" \
 | awk -F: '{print $NF}' \
@@ -22,10 +23,13 @@ curl --retry 2 --retry-delay 8 --max-time 5 -sS "${URL_iCalGoogle_JHoliday}" \
 | awk '$1 >= '${TODAY_YYYYMMDD}' {print $0}'
 
 echo
+sleep 1
 
-curl --retry 2 --retry-delay 8 --max-time 5 -sS "${URL_csvCAO_JHoliday}" \
+curl -sSm 5 "${URL_csvCAO_JHoliday}" \
 | tr -d '\r' \
 | sed -n '2,$p' \
 | nkf -w \
 | perl -pe 's/^(2...)-(..)-(..),/$1$2$3 /' \
 | awk '$1 >= '${TODAY_YYYYMMDD}' {print $0}'
+
+exit

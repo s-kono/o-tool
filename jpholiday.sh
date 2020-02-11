0cb12a74e93c2d93c7dd0f42d69d515779f403a9
s-kono <s-kono@outlook.com>
2020-02-11 19:33:16 +0900

    update jpholiday.sh

--- jpholiday.sh
+++ jpholiday.sh
@@ -3,14 +3,14 @@
 set -u
 
 export LANG=C
-export LC_CTYPE=en_US.UTF-8
+export LC_CTYPE=en_US.utf8
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 readonly URL_iCalGoogle_JHoliday="https://calendar.google.com/calendar/ical/ja.japanese%23holiday@group.v.calendar.google.com/public/basic.ics"
 # https://ja.wikipedia.org/wiki/ICalendar
 
-readonly URL_csvCAO_JHoliday=http://www8.cao.go.jp/chosei/shukujitsu/syukujitsu_kyujitsu.csv
-# http://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html
+readonly URL_csvCAO_JHoliday=https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv
+# https://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html
 
 readonly TODAY_YYYYMMDD=$( date +%Y%m%d )
 
@@ -22,14 +22,16 @@ curl -sSm 5 "${URL_iCalGoogle_JHoliday}" \
 | sort -n \
 | awk '$1 >= '${TODAY_YYYYMMDD}' {print $0}'
 
-echo
 sleep 1
 
 curl -sSm 5 "${URL_csvCAO_JHoliday}" \
 | tr -d '\r' \
 | sed -n '2,$p' \
 | nkf -w \
-| perl -pe 's/^(2...)-(..)-(..),/$1$2$3 /' \
+| tr , ' ' \
+| while read _d _n; do
+    echo "$( date +%Y%m%d -d${_d} ) ${_n}"
+done \
 | awk '$1 >= '${TODAY_YYYYMMDD}' {print $0}'
 
 exit
